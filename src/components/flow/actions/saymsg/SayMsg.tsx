8860910aa55c99e0860a3affc0e1b076aea32daf
Ihor Palii <palii.ihor@uvik.net>
2024-07-08 16:21:34 +0300

    Updated flowaction

--- src/components/flow/actions/saymsg/SayMsg.tsx
+++ src/components/flow/actions/saymsg/SayMsg.tsx
@@ -8,10 +8,28 @@ import i18n from 'config/i18n';
 export const PLACEHOLDER = i18n.t('actions.say_msg.placeholder', 'Send a message to the contact');
 
 const SayMsgComp: React.SFC<SayMsg> = (action: SayMsg): JSX.Element => {
+  const getUploadedFileName = () => {
+    if (action.audio_url && action.audio_url.length > 0) {
+      let fullPath = action.audio_url;
+      let fullPathSplit = fullPath.split('/');
+      return fullPathSplit[fullPathSplit.length - 1];
+    }
+    return '';
+  };
+
   if (action.text) {
     return (
       <>
-        <div className={styles.text}>{action.text}</div>
+        <div className={styles.text}>
+          {action.text}
+          {action.audio_url && (
+            <>
+              <br />
+              <div className={`${styles.attachment} fe-paperclip`} />
+              {getUploadedFileName()}
+            </>
+          )}
+        </div>
 
         {action.audio_url ? (
           <div className={styles.recording}>
